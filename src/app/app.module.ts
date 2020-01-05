75bd9b6fe9647693385c71d43c943f09f754eb0b
Hristo Gatsinski <gatsinski@gmail.com>
2020-01-05 17:52:49 +0200

    Add Clarity Design System

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ClarityModule } from '@clr/angular';
 @NgModule({
   declarations: [
     AppComponent
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    ClarityModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
